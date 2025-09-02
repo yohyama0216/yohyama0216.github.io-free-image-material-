b67318f60baa179d52a5c5bdf12523741e68e5d7
yohyama <yohyama0216>
2025-09-02 22:52:21 +0900

    Fix GitHub Pages routing: add .nojekyll file and optimize config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const nextConfig = {
   },
   basePath: '/yohyama0216.github.io-free-image-material-',
   assetPrefix: '/yohyama0216.github.io-free-image-material-/',
+  // Ensure proper file generation for GitHub Pages
+  distDir: 'out',
 }
 
 module.exports = nextConfig
